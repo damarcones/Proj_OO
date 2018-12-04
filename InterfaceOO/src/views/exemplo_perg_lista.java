d310edab0c148b31e869b64034826841a8bf0060
Damarcones <damarcones>
2018-12-03 22:59:20 -0200

    Linkando tela de exemplo de lista

--- InterfaceOO/src/views/exemplo_perg_lista.java
+++ InterfaceOO/src/views/exemplo_perg_lista.java
@@ -16,6 +16,7 @@ public class exemplo_perg_lista extends javax.swing.JFrame {
      */
     public exemplo_perg_lista() {
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -42,6 +43,11 @@ public class exemplo_perg_lista extends javax.swing.JFrame {
         jLabel1.setText("Exemplo de pergunta com resposta do tipo lista");
 
         jButton1.setText("Voltar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
@@ -115,6 +121,11 @@ public class exemplo_perg_lista extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        new exemplos().setVisible(true);
+        dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
